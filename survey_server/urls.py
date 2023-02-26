a5b818016bdd5434c596f2a522b075d16cce80c9
Lucia Berecova <2271766b@student.gla.ac.uk>
2023-02-26 14:46:00 +0000

    url patterns & static tag fix

--- survey_server/urls.py
+++ survey_server/urls.py
@@ -5,8 +5,8 @@ app_name = 'survey_server'
 
 urlpatterns = [
 path('', views.index, name='index'),
-path('/customer', views.customer, name='customer'),
-path('/manager', views.manager, name='manager'),
-path('/profile', views.profile, name='profile'),
-path('/question1', views.question1, name='question1'),
+path('customer/', views.customer, name='customer'),
+path('manager/', views.manager, name='manager'),
+path('profile/', views.profile, name='profile'),
+path('question1/', views.question1, name='question1'),
 ]
