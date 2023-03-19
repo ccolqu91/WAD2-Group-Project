2906d01dc217f2b3379aaebd0881009aba84dea7
fu shaopei <2721871f@student.gla.ac.uk>
2023-03-19 19:08:38 +0000

    made some changes in js file, but doesnt finish yet

--- survey_server/urls.py
+++ survey_server/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from survey_server import views
-
-
+              
+              
 app_name = 'survey_server'
-
-
+             
+             
 urlpatterns = [
 path('', views.index, name='index'),
 path('customer/', views.customer, name='customer'),
@@ -19,4 +19,6 @@ path('select/', views.select_restaurant, name='select'),
 path('about/', views.about, name='about'),
 path('add_restaurant/', views.add_restaurant, name='add_restaurant'),
 path('edit_restaurant/', views.edit_restaurant, name='edit_restaurant'),
+
+
 ]
