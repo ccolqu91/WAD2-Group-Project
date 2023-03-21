2f25f61f168e520151a3d8da5fa5947425730121
fu shaopei <2721871f@student.gla.ac.uk>
2023-03-21 12:28:17 +0000

    changed manager.html profile picture, changed it back

--- static/js/myChart.js
+++ static/js/myChart.js
@@ -1,5 +1,5 @@
 
-
+// fake data just in case.
 var ctx = document.getElementById('myChart').getContext('2d');
         var data = {
             labels: ['Food Quality', 'Customer Service', 'Hygiene', 'Value for Money', 'Menu Variety'],
@@ -41,34 +41,37 @@ var ctx = document.getElementById('myChart').getContext('2d');
             options: options
         });
 
-var ctx = document.getElementById("myChart").getContext('2d');
-        var  charData= {
-                labels: ['Food Quality', 'Customer Service', 'Hygiene', 'Value for Money', 'Menu Variety'],
-                datasets: [{
-                    label: 'Average Score',
-                    data: JSON.parse('{{ avg_scores_json }}'),
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)'
-                      ],
-                      borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)'
-                      ],
-                      borderWidth: 1
 
 
-                }]
-            };
+// Another possible idea
+// var ctx = document.getElementById("myChart").getContext('2d');
+//         var  charData= {
+//                 labels: ['Food Quality', 'Customer Service', 'Hygiene', 'Value for Money', 'Menu Variety'],
+//                 datasets: [{
+//                     label: 'Average Score',
+//                     data: JSON.parse('{{ avg_scores_json }}'),
+//                     backgroundColor: [
+//                         'rgba(255, 99, 132, 0.2)',
+//                         'rgba(54, 162, 235, 0.2)',
+//                         'rgba(255, 206, 86, 0.2)',
+//                         'rgba(75, 192, 192, 0.2)',
+//                         'rgba(153, 102, 255, 0.2)'
+//                       ],
+//                       borderColor: [
+//                         'rgba(255, 99, 132, 1)',
+//                         'rgba(54, 162, 235, 1)',
+//                         'rgba(255, 206, 86, 1)',
+//                         'rgba(75, 192, 192, 1)',
+//                         'rgba(153, 102, 255, 1)'
+//                       ],
+//                       borderWidth: 1
 
-        var myChart = new Chart(ctx,{
-            type:'bar',
-            data:charData,
-            oprions:{}
-        });
+
+//                 }]
+//             };
+
+//         var myChart = new Chart(ctx,{
+//             type:'bar',
+//             data:charData,
+//             oprions:{}
+//         });
