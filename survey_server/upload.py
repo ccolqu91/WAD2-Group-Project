2f1fdf39bd307f04b3c1614d00bff492daa1b78b
Lucia Berecova <2271766b@student.gla.ac.uk>
2023-03-24 14:15:42 +0000

    added code back to avoid old migration errors

--- survey_server/upload.py
+++ survey_server/upload.py
@@ -7,3 +7,32 @@ def validate_file_extension(value):
     if not value.name.endswith('.csv'):
         raise ValidationError('Only CSV files are allowed.')
 
+def menu_upload_to(instance, filename):
+    media_path = settings.MEDIA_ROOT
+    media_path= media_path.replace("\\","/")
+    parent_directory_path = os.path.join(media_path, 'menus')
+
+    if not os.path.exists(parent_directory_path):
+        os.makedirs(parent_directory_path)
+
+    directory_path = os.path.join(parent_directory_path, instance.slug)
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    file_path = os.path.join(directory_path, filename)
+    return file_path
+
+def logo_upload_to(instance, filename):
+    media_path = settings.MEDIA_ROOT
+    media_path= media_path.replace("\\","/")
+    parent_directory_path = os.path.join(media_path, 'logos')
+
+    if not os.path.exists(parent_directory_path):
+        os.makedirs(parent_directory_path)
+
+    directory_path = os.path.join(parent_directory_path, instance.slug)
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    file_path = os.path.join(directory_path, filename)
+    return file_path
